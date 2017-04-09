6a525a52bc23b16947116f5f24bff180d505d92a
Przemysław Szurek <przemekszurek@gmail.com>
2017-04-09 21:50:33 +0200

    Add JWT authentication example

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,25 @@
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by przemek on 2017-03-19.
  */
 
 @SpringBootApplication
-@ComponentScan({"service", "controller"}) // If class in your package doesn't work add such package here.
+@RestController
+@EnableAutoConfiguration
+@ComponentScan({"service", "controller", "security"}) // If class in your package doesn't work add such package here.
 public class Application {
+
+    @RequestMapping("/")
+    String hello() {
+        return "hello world";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ src/main/java/security/AccountCredentials.java
@@ -0,0 +1,19 @@
+package security;
+
+/**
+ * Created by przemek on 2017-04-09.
+ */
+
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+    // getters & setters
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
