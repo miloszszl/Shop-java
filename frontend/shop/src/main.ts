e3faf47ea8de3abce4387a3e238d2fb4e7defc78
Przemysław Szurek <przemekszurek@gmail.com>
2017-04-09 21:57:53 +0200

    Add JWT login example with fake backend and webpack

--- frontend/shop/src/main.ts
+++ frontend/shop/src/main.ts
@@ -1,11 +1,10 @@
+﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
+if (process.env.ENV === 'production') {
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
